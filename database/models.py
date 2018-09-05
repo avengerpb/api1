03d36cfaf728f0bf894c833734440e32974da42d
Dat Le <datlequoc96@gmail.com>
2018-09-05 15:04:26 +0300

    Registered and DB models

--- database/models.py
+++ database/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Text, DateTime
 from sqlalchemy.orm import relationship
 
 Base = declarative_base()
@@ -8,12 +8,24 @@ class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     user_name = Column(String(250), nullable=False)
+    password =  Column(String(250), nullable=False)
 
 class User_Info(Base):
-    __tablename__ = 'userInfo'
+    __tablename__ = 'user_info'
     id = Column(Integer, primary_key=True)
-    user_street_name = Column(String(250))
-    user_street_number = Column(String(250))
-    user_post_code = Column(String(250), nullable=False)
+    user_email = Column(String(250), nullable=False)
+    user_phone = Column(String(250))
+    user_address = Column(String(250))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+class Note(Base):
+    __tablename__ = 'note'
+    id = Column(Integer, primary_key=True)
+    content = Column(Text, nullable=False)
+    image_url = Column(String(250))
+    created_at = Column(DateTime)
+    update_at = Column(DateTime)
+    status = Column(Integer, nullable=False, server_default='0')
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
--- run.py
+++ run.py
@@ -3,10 +3,9 @@ from flask import request, jsonify
 import sys
 import logging
 from database import *
+import secrets, string
 
 app = Flask(__name__)
-
-
 
 @app.before_request
 def before_request():
@@ -18,23 +17,37 @@ def after_request():
 
 @app.route('/', methods=['GET'])
 def home():
+    return "Hi"
+
+@app.route('/register', methods=['POST'])
+def register():
+    user_name = request.form['user_name']
+    user_email = request.form['user_email']
+    alphabet = string.ascii_letters + string.digits
+    gpassword = ''.join(secrets.choice(alphabet) for i in range(8))
+
     user = User(
-        user_name = 'Dat Le'
+        user_name = user_name,
+        password = gpassword
         )
+
     session.add(user)
+    session.flush()
+
+    info = User_Info(
+        user_id = user.id,
+        user_email = user_email
+        )
+
+    session.add(info)
     try:
          session.commit()
-         return " OK Fine"
+         return "User Registered"
     except Exception as e:
        #log your exception in the way you want -> log to file, log as error with default logging, send by email. It's upon you
        session.rollback()
        session.flush() # for resetting non-commited .add()
        failed=True
-
-
-@app.route('/api/v1/resources/books/all', methods=['GET'])
-def api_all():
-    return jsonify(books)
 
 
 if __name__ == '__main__':
